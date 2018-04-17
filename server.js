05d4360227c0a925853840edbdac70b17e3ed2d7
Dragoteryx <33252782+Dragoteryx@users.noreply.github.com>
2018-04-17 13:36:55 +0200

    now using express

--- server.js
+++ server.js
@@ -1,112 +1,144 @@
 "use strict";
 
 require("dotenv").config();
-const http = require("http");
+const express = require("express");
 const url = require("url");
 const cp = require("child_process");
 const facts = require("./src/facts.js");
 
-http.createServer(async (req, res) => {
-  let parsed = url.parse(req.url, true);
-	let query = parsed.query;
-  let authorized = query.auth == process.env.AUTHTOKEN;
-	let now = Date.now();
-  if (parsed.path != "/favicon.ico")
-    console.log(parsed.path + (authorized ? " (authorized)" : ""));
-  if (parsed.pathname == "/") {
-    res.writeHead(301, {Location: "/generate"});
-    res.end();
-  } else if (parsed.pathname == "/generate") {
-    res.writeHead(200, {"Content-Type": "application/json"});
-    if (query.includes === undefined)
-    	res.end(JSON.stringify({fact: await facts.genFact(), found: true, duration: (Date.now() - now), tries: 1}));
-  	else {
-  		let child = cp.fork("./src/find.js");
-  		child.on("message", fact => {
-  			res.end(JSON.stringify({fact: fact.text, found: fact.found, duration: (Date.now() - now), tries: fact.tries}));
-  			child.kill();
-  		});
-  		child.on("close", () => {
-  			console.log("Find child killed");
-  		});
-  		child.send(query.includes);
-  	}
-  } else if (parsed.pathname == "/database") {
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify(await facts.fetchDatabase()));
-  } else if (parsed.pathname == "/bulk") {
-    res.writeHead(200, {"Content-Type": "application/json"});
-    if (query.nb === undefined) query.nb = 100;
-    let child = cp.fork("./src/bulk.js");
-    child.on("message", bulk => {
-      res.end(JSON.stringify(bulk));
-      child.kill();
-    });
-    child.on("close", () => {
-      console.log("Bulk child killed");
-    });
-    child.send(query.nb);
-  } else if (parsed.pathname == "/insert") {
-    if (authorized && query.alias !== undefined && query.string !== undefined) {
-      res.writeHead(302, {Location: "/database"});
-      res.end();
-      let database = await facts.fetchDatabase();
-      while (query.string.includes("_"))
-        query.string = query.string.replace("_", " ");
-      database.forEach(cat => {
-        if (cat.alias == query.alias) {
-          cat.strings.push(query.string);
-          console.log("Inserted '" + query.string + "' into '" + cat.alias + "'");
-        }
-      });
-      facts.provideDatabase(database);
+const app = express();
+
+app.get("/", async (req, res) => {
+  res.status(301).setHeader("Location", "/generate");
+  res.end();
+});
+
+app.get("/generate", async (req, res) => {
+  let now = Date.now();
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({fact: await facts.genFact(), found: true, duration: (Date.now() - now), tries: 1}));
+});
+
+app.get("/generate/:includes", async (req, res) => {
+  let now = Date.now();
+  res.setHeader("Content-Type", "application/json");
+  let child = cp.fork("./src/find.js");
+  child.on("message", fact => {
+    res.send(JSON.stringify({fact: fact.text, found: fact.found, duration: (Date.now() - now), tries: fact.tries}));
+    child.kill();
+  });
+  child.on("close", () => {
+    console.log("Find child killed");
+  });
+  child.send(req.params.includes);
+});
+
+app.get("/database", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(await facts.fetchDatabase()));
+});
+
+function bulk(nb, res) {
+  let child = cp.fork("./src/bulk.js");
+  child.on("message", bulk => {
+    res.send(JSON.stringify(bulk));
+    child.kill();
+  });
+  child.on("close", () => {
+    console.log("Bulk child killed");
+  });
+  child.send(nb);
+}
+
+app.get("/bulk", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  bulk(100, res);
+});
+
+app.get("/bulk/:nb", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  bulk(req.params.nb, res);
+});
+
+app.get("/insert/:alias/:string", async (req, res) => {
+  res.writeHead(302, {Location: "/database"});
+  let database = await facts.fetchDatabase();
+  let query = {
+    alias: req.params.alias,
+    string: req.params.string
+  };
+  while (query.string.includes("_"))
+    query.string = query.string.replace("_", " ");
+  if (!database.some(cat => cat.alias == query.alias))
+    database.push({alias: query.alias, strings: []});
+  database.forEach(cat => {
+    if (cat.alias == query.alias) {
+      cat.strings.push(query.string);
+      console.log("Inserted '" + query.string + "' into '" + cat.alias + "'");
     }
-  } else if (parsed.pathname == "/remove") {
-    if (authorized && query.alias !== undefined && query.string !== undefined) {
-      res.writeHead(302, {Location: "/database"});
-      res.end();
-      let database = await facts.fetchDatabase();
-      while (query.string.includes("_"))
-        query.string = query.string.replace("_", " ");
-      database.forEach(cat => {
-        if (cat.alias == query.alias) {
-          cat.strings = cat.strings.filter(str => str != query.string);
-          console.log("Removed '" + query.string + "' from '" + cat.alias + "'");
-        }
-      });
-      facts.provideDatabase(database);
+  });
+  facts.provideDatabase(database);
+  res.end();
+});
+
+app.get("/remove/:alias/:string", async (req, res) => {
+  res.writeHead(302, {Location: "/database"});
+  let database = await facts.fetchDatabase();
+  let query = {
+    alias: req.params.alias,
+    string: req.params.string
+  };
+  while (query.string.includes("_"))
+    query.string = query.string.replace("_", " ");
+  database.forEach(cat => {
+    if (cat.alias == query.alias) {
+      cat.strings = cat.strings.filter(str => str != query.string);
+      console.log("Removed '" + query.string + "' from '" + cat.alias + "'");
     }
-  } else if (parsed.pathname == "/replace") {
-    if (authorized && query.before !== undefined && query.after !== undefined) {
-      res.writeHead(302, {Location: "/database"});
-      res.end();
-      let database = await facts.fetchDatabase();
-      while (query.before.includes("_"))
-        query.before = query.before.replace("_", " ");
-      while (query.after.includes("_"))
-        query.after = query.after.replace("_", " ");
-      let str = JSON.stringify(database);
-      while (str.includes(query.before))
-        str = str.replace(query.before, query.after);
-      facts.provideDatabase(JSON.parse(str));
-    }
-  } else if (parsed.pathname == "/reset") {
-    res.writeHead(302, {Location: "/database"});
-    res.end();
-    if (authorized) {
-      console.log("Database reset.");
-      facts.provideDatabase(facts.saved);
-    }
-  } else if (parsed.pathname == "/delete") {
-    res.writeHead(302, {Location: "/database"});
-    res.end();
-    if (authorized && query.alias !== undefined) {
-      let database = await facts.fetchDatabase();
-      database = database.filter(cat => cat.alias != query.alias);
-      console.log("Deleted alias '" + query.alias + "' from database.");
-      facts.provideDatabase(database);
-    }
-  }
-}).listen(process.env.PORT);
+  });
+  facts.provideDatabase(database);
+  res.end();
+});
+
+app.get("/replace/:before/:after", async (req, res) => {
+  res.writeHead(302, {Location: "/database"});
+  let database = await facts.fetchDatabase();
+  let query = {
+    before: req.params.before,
+    after: req.params.after
+  };
+  while (query.before.includes("_"))
+    query.before = query.before.replace("_", " ");
+  while (query.after.includes("_"))
+    query.after = query.after.replace("_", " ");
+  let str = JSON.stringify(database);
+  while (str.includes(query.before))
+    str = str.replace(query.before, query.after);
+  facts.provideDatabase(JSON.parse(str));
+  res.end();
+});
+
+app.get("/reset", async (req, res) => {
+  res.writeHead(302, {Location: "/database"});
+  console.log("Database reset.");
+  facts.provideDatabase(facts.saved);
+  res.end();
+});
+
+app.get("/delete/:cat", async (req, res) => {
+  res.writeHead(302, {Location: "/database"});
+  let database = await facts.fetchDatabase();
+  database = database.filter(cat => cat.alias != req.params.cat);
+  console.log("Deleted category '" + req.params.cat + "' from database.");
+  facts.provideDatabase(database);
+  res.end();
+});
+
+app.use(async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(404).send(JSON.stringify({}));
+});
+
+app.listen(process.env.PORT);
 
 console.log("Web server ready!");
